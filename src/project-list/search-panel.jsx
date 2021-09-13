e83f9127de71687bcd8264cc14462ee5a564f7a1
EvenW <yuhang0229@gmail.com>
2021-09-13 21:26:28 +1000

    add .env

--- src/project-list/search-panel.jsx
+++ src/project-list/search-panel.jsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const SearchPanel = () => {
+  const [param, setParam] = useState();
+  const [managers, setManagers] = useState([]);
+  /*
+  const getInput= (e) => {
+    setParam({
+      ...param,
+      project_name: e
+    });
+  };
+
+  const getSelection = (e) => {
+    setParam({
+      ...param,
+      manager_id:e.target.value
+    })
+  }
+*/
+  useEffect(() => {
+    fetch(`${apiUrl}`).then(async (response) => {
+      if (response.ok) {
+        setManagers(await response.json());
+      }
+    });
+  });
+
   return (
     <form>
-      <input type="text" />
-      <select name="" id=""></select>
+      <input
+        type="text"
+        value={param.project_name}
+        onChange={(e) => {
+          setParam({
+            ...param,
+            project_name: e,
+          });
+        }}
+      />
+      <select
+        value={param.manager_id}
+        onChange={(e) => {
+          setParam({
+            ...param,
+            manager_id: e.target.value,
+          });
+        }}
+      >
+        <option value="">Manager</option>
+      </select>
     </form>
   );
 };
